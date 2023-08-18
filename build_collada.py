ce970c0fdcb29ea7e03be464bbce1037b210aff8
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2023-08-18 14:46:45 -0700

    Catch vertex groups missing from vgmap

--- build_collada.py
+++ build_collada.py
@@ -521,10 +521,15 @@ def add_geometries_and_controllers (collada, submeshes, skeleton, materials, has
                             new_weight.append(blendweights[i][j])
                             new_index.append(local_to_global_joints[blendindices[i][j]])
                         except KeyError:
-                            missing_bone = [x for x in submesh['vgmap'].keys() if submesh['vgmap'][x] == blendindices[i][j]][0]
-                            print("KeyError: Attempted to map {1} to skeleton while adding submesh {0} to COLLADA, but {1} does not exist in the heirachy!".format(submesh["name"], missing_bone))
-                            input("Press Enter to abort.")
-                            raise
+                            try:
+                                missing_bone = [x for x in submesh['vgmap'].keys() if submesh['vgmap'][x] == blendindices[i][j]][0]
+                                print("KeyError: Attempted to map {1} to skeleton while adding submesh {0} to COLLADA, but {1} does not exist in the heirachy!".format(submesh["name"], missing_bone))
+                                input("Press Enter to abort.")
+                                raise
+                            except IndexError:
+                                print("IndexError: Vertex attempted to use group {1} while adding submesh {0} to COLLADA, but group {1} does not exist in the vgmap!".format(submesh["name"], blendindices[i][j]))
+                                input("Press Enter to abort.")
+                                raise
                 new_weights.append(new_weight)
                 new_indices.append(new_index)
             #Uncomment the next 3 lines to force local bones instead of global bones
