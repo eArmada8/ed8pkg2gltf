3b61bb6d04928e232c1e2b31c82da37bada2293a
bernd7055 <b9772647@gmail.com>
2025-08-23 19:41:21 +0200

    find_similar_shaders.py: add weights flags for kinds of differences
    This change removes the -it and -nt flag in favor of a -w flag that
    allows users to specify a dictionary of weights to give different types
    of differences (e.g. texture_slot) different weights. This allows users
    to specify they own metric for similarity and thus affect the ordering
    in which the script returns the shaders (especially useful with the -nr
    flag which return the most similar shader).
    
    This also optimized the lookup to first filter the shaders based on the
    game restrictions and then compute the similarity and order the shaders.
    Before this change the script was computing and ordering all shaders and
    then filtered afterwards.
    
    I tested this on 50 random shaders for all games and the new and old
    versions return exactly the same results for all of them (if -w is not
    set).

--- misc/find_similar_shaders.py
+++ misc/find_similar_shaders.py
@@ -8,17 +8,22 @@
 # GitHub eArmada8/ed8pkg2gltf
 
 import os, csv
+from typing import Dict
 import argparse
 
 csv_file = 'all_shaders.csv'
 
+def is_texture_slot(switch: str) -> bool:
+    return "MAPPING" in switch or "MULTI_UV" in switch or "_MAP_ENABLED" in switch
+
 class Shader_db:
-    def __init__(self, shader_db_csv, report_file = 'report.txt'):
+    def __init__(self, shader_db_csv, weights, report_file = 'report.txt'):
         self.shader_db_csv = shader_db_csv
         self.report_file = report_file
         self.shader_array = self.read_shader_csv()
         self.shader_switches = self.shader_array[0][6:]
         self.shader_sig = {x[0]:''.join(x[6:]) for x in self.shader_array[1:]}
+        self.weights = weights
         self.diffs = {}
         self.restriction = ''
         self.restriction_column = None
@@ -46,11 +51,23 @@ class Shader_db:
         differences = [i for i in range(len(string1)) if string1[i] != string2[i]]
         return({self.shader_switches[i]:string2[i] for i in differences})
 
+    def weighted_diff(self, shader1, shader2):
+        diff = self.diff(shader1, shader2)
+        weight = 0
+        for k,vs in diff.items():
+            v = int(vs)
+            weights = self.weights['other_switch']
+            if is_texture_slot(k):
+                weights = self.weights['texture_slot']
+            weight += weights['removed'] * (not v)
+            weight += weights['added'] * v
+        return weight
+
     def sort_shaders_by_similarity(self, shader):
-        shader_diff = {k:(int(self.shader_sig[k], base=2) ^ int(self.shader_sig[shader], base=2)).bit_count() \
-            for k in self.shader_sig if k != shader}
-        diff_val = sorted(list(set(shader_diff.values())))
-        self.diffs = {diff_val[i]:{x:self.diff(shader,x) for x in shader_diff if shader_diff[x] == diff_val[i]}\
+        shader_diffs = {k: self.weighted_diff(shader, k) \
+            for k in self.restricted_list if k != shader}
+        diff_val = sorted(list(set(shader_diffs.values())))
+        self.diffs = {diff_val[i]:{x:self.diff(shader,x) for x in shader_diffs if shader_diffs[x] == diff_val[i]}\
             for i in range(len(diff_val))}
 
     def format_report(self, shader):
@@ -59,18 +76,16 @@ class Shader_db:
             self.report += '\nRestriction: {} is not None\n'.format(self.restriction)
         array_first_column = [x[0] for x in self.shader_array]
         for i in self.diffs:
-            if len([j for j in self.diffs[i] if j in self.restricted_list]) > 0:
-                self.report += '\nShaders with {} differences:\n\n'.format(i)
-                for j in self.diffs[i]:
-                    if j in self.restricted_list:
-                        self.report += '{}:'.format(j)
-                        if self.restriction_column != None:
-                            row = array_first_column.index(j)
-                            self.report += ' (available in {})\n'.format(self.shader_array[row][self.restriction_column])
-                        else:
-                            self.report += '\n'
-                        self.report += '\n'.join(['{0}: {1}'.format(k,v) for (k,v)\
-                            in self.diffs[i][j].items()]) + '\n\n'
+            self.report += '\nShaders with {} differences:\n\n'.format(i)
+            for j in self.diffs[i]:
+                self.report += '{}:'.format(j)
+                if self.restriction_column != None:
+                    row = array_first_column.index(j)
+                    self.report += ' (available in {})\n'.format(self.shader_array[row][self.restriction_column])
+                else:
+                    self.report += '\n'
+                self.report += '\n'.join(['{0}: {1}'.format(k,v) for (k,v)\
+                    in self.diffs[i][j].items()]) + '\n\n'
         return(self.report)
 
     def generate_report(self, shader, write_file):
@@ -80,25 +95,47 @@ class Shader_db:
                 f.write(self.format_report(shader))
         else:
             for i in self.diffs:
-                if len([j for j in self.diffs[i] if j in self.restricted_list]) > 0:
-                    for j in self.diffs[i]:
-                        if j in self.restricted_list:
-                            # print any since we are lacking a good heuristic for the "best"
-                            print(j)
-                            return
+                for j in self.diffs[i]:
+                    # Print the least different (according to the weights).
+                    print(j)
+                    return
         return
+
+def parse_weight_dict(input: str) -> Dict[str, Dict[str, int]]:
+    weights = {
+      "texture_slot": {
+        "added": 1,
+        "removed": 1,
+      },
+      "other_switch": {
+        "added": 1,
+        "removed": 1,
+      },
+    }
+    if input == "":
+        return weights
+    for w in input.split(','):
+        m = w.split('=')
+        if (len(m) != 3):
+            raise argparse.ArgumentTypeError(
+            f"Invalid weights dictionary format: '{input}'. "
+            "Expected format: type=(added|removed)=weight"
+            )
+        weights[m[0]][m[1]] = int(m[2])
+    return weights
 
 if __name__ == "__main__":
     # Set current directory
     os.chdir(os.path.abspath(os.path.dirname(__file__)))
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--shader', type=str, help="shader to find, e.g.ed8.fx#CE03DCE5DFEF5F7C4FB6937519B03034")
-    parser.add_argument('-g', '--game', type=str, help="game in which to find a similar shader, any of {cs1,cs2,cs3,cs4,cs5}")
-    parser.add_argument('--no-report', action='store_false', dest='write_file', help="prints a single most similar shader to stdout (any if there are multiple)")
+    parser.add_argument('-s', '--shader', type=str, help="Shader to find, e.g.ed8.fx#CE03DCE5DFEF5F7C4FB6937519B03034")
+    parser.add_argument('-g', '--game', type=str, help="Game in which to find a similar shader, any of {cs1,cs2,cs3,cs4,cs5}")
+    parser.add_argument('-w', '--weights', type=parse_weight_dict, help="A dictionary from switch type to, added or removed to custom weight. Unspecified weights will use 1 as default. Larger weight means larger difference. Available types are: [texture_slot, other_switch]. Format: type=(added|removed)=weight.", default = "")
+    parser.add_argument('-nr', '--no-report', action='store_false', dest='write_file', help="prints a single most similar shader to stdout (any if there are multiple)")
     args = parser.parse_args()
 
     if os.path.exists(csv_file):
-        shader_db = Shader_db(csv_file)
+        shader_db = Shader_db(csv_file, args.weights, 'report.txt')
         shader = args.shader
         if not shader:
             shader = input("Please enter name of shader to analyze: ")
